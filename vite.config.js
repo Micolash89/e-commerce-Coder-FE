382ba8bf02a50fae672119c50b2661e52c417f88
javier <espindolajavier2013@gmail.com>
2024-04-08 17:05:56 -0300

    Update base path in vite.config.js
    Change the `base` path from "/e-commerce-Coder-FE/" to "/e-commerce-Coder-FrontEnd/".

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/e-commerce-Coder-FE/"
+  base: "/e-commerce-Coder-FrontEnd/"
 })
