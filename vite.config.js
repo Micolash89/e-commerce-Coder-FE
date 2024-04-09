0339ecde83d166bf1e40ea3c643f794d41cb3591
javier <espindolajavier2013@gmail.com>
2024-04-08 21:32:19 -0300

    Change base path to include protocol and host for local development
    The base path in the Vite configuration has been updated to include the protocol and host (localhost:4173) for local development.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/e-commerce-Coder-FrontEnd/"
+  base: "http://localhost:4173/e-commerce-Coder-FrontEnd/"
 })
