ac927f6cec7251d3f84302c16a765530a39c993f
javier <espindolajavier2013@gmail.com>
2024-04-08 21:41:41 -0300

    Change base URL to deployed site address
    The base URL in the Vite configuration file has been updated from "http://localhost:4173/e-commerce-Coder-FrontEnd/" to "<https://e-commerce-coder-frontend.onrender.com>". This change will make the application use the deployed site address instead of the local development server.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://localhost:4173/e-commerce-Coder-FrontEnd/"
+  base: "https://e-commerce-coder-frontend.onrender.com"
 })
