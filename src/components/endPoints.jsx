9219070b4fb15d6d440454693383dc52f154f8e0
javier <espindolajavier2013@gmail.com>
2024-05-02 11:15:09 -0300

    Change NODE\_ENV from 'dev' to 'production' and update PROD endpoint
    * Changed `NODE_ENV` constant value from "dev" to "production"
    * Updated `PROD` endpoint to point to the

--- src/components/endPoints.jsx
+++ src/components/endPoints.jsx
@@ -1,5 +1,5 @@
 export const END_POINTS = {
-  NODE_ENV: "dev",
+  NODE_ENV: "production",
   PROD: "https://e-commerce-nodejs-2-4qw3.onrender.com",
   DEV: "http://localhost:8080",
   URL: function () {
